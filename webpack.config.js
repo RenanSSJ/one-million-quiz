8c7b7ff5a1f6d8473d410b0665876a9301402ce8
Renan <renansantos.trab@gmail.com>
2025-09-10 12:00:02 +0100

    Webpack config + loaders setup

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development', 
+  entry: path.resolve(__dirname, 'src/js/index.js'),
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+    publicPath: '/',
+  },
+  devServer: {
+      port: 3000,
+      open: true,
+      liveReload: true,
+      // Rewrites URLS - redirect to the correct pages 
+      historyApiFallback: {
+        rewrites: [
+          { from: /^\/$/, to: '/index.html' },
+          { from: /^\/question$/, to: '/question/index.html' },
+          { from: /^\/winner$/, to: '/winner/index.html' }
+        ]
+      },
+    },
+
+    plugins: [
+      // Gen Index.html
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+        filename: 'index.html'
+      }),
+
+      // Gen /question/index.html for /question)
+      new HtmlWebpackPlugin({
+        template: './src/pages/question/index.html',
+        filename: 'question/index.html'
+      }),
+
+      // Gen /winner/index.html for /winner)
+      new HtmlWebpackPlugin({
+        template: './src/pages/winner/index.html',
+        filename: 'winner/index.html'
+      })
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"]
+        }
+      ]
+    }
+
+}
