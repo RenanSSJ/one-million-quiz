1a3849286288677a9ccdbb52c28f8ede83aa422c
Renan <renansantos.trab@gmail.com>
2025-10-14 19:40:24 +0100

    feat: redirect to questions page when start is pressed and if names are valid.

--- src/js/index.js
+++ src/js/index.js
@@ -35,8 +35,7 @@ startButton.addEventListener("click", () => {
     localStorage.setItem("player1", player1);
     localStorage.setItem("player2", player2)
 
-    console.log(player1);
-    console.log(player2);
+    window.location.href = "/question";
   }
 });
 
